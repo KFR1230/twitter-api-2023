6e3e703ebd1941173c90978d054b957fbccfa633
KFR1230 <su0916111448@gmail.com>
2023-08-04 20:53:54 +0800

    remove if

--- app.js
+++ app.js
@@ -1,18 +1,16 @@
-if (process.env.NODE_ENV === 'production') {
-  require('dotenv').config()
-}
-const path = require('path')
-const express = require('express')
-const routes = require('./routes')
-const methodOverride = require('method-override')
-const app = express()
-const port = process.env.PORT || 3000
-const cors = require('cors')
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const routes = require('./routes');
+const methodOverride = require('method-override');
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
 
-app.use(express.urlencoded({ extended: true }))
-app.use('/upload', express.static(path.join(__dirname, 'upload')))
-app.use(methodOverride('_method'))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use('/upload', express.static(path.join(__dirname, 'upload')));
+app.use(methodOverride('_method'));
+app.use(express.json());
 
 app.use(
   cors({
@@ -20,12 +18,12 @@ app.use(
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
-    preflightContinue: false
+    preflightContinue: false,
   })
-)
+);
 
-app.use(routes)
-app.get('/', (req, res) => res.send('Hello Kitty - Users/Tweets Done'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use(routes);
+app.get('/', (req, res) => res.send('Hello Kitty - Users/Tweets Done'));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-module.exports = app
+module.exports = app;
