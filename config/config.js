8a18953bb2a15e4d171a78f36571104eceddc91e
KFR1230 <su0916111448@gmail.com>
2023-08-04 15:20:55 +0800

    change dotenv

---
+++ config/config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  development: {
+    username: 'root',
+    password: 'password',
+    database: 'ac_twitter_workspace',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false,
+  },
+  test: {
+    username: 'root',
+    password: 'password',
+    database: 'ac_twitter_workspace_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false,
+  },
+  production: {
+    username: 'process.env.DB_USERNAME',
+    password: 'process.env.DB_PASSWORD',
+    database: 'process.env.DB_NAME',
+    host: 'process.env.DB_HOST',
+    port: 'process.env.DB_PORT',
+    dialect: 'mysql',
+  },
+  travis: {
+    username: 'travis',
+    database: 'ac_twitter_workspace_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    logging: false,
+  },
+  ghactions: {
+    username: 'root',
+    database: 'ac_twitter',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  }
+}
