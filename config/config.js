ecfaa39af38f823a4ad2f0aa8fd3d96f5146494c
KFR1230 <su0916111448@gmail.com>
2023-08-05 10:16:30 +0800

    modify config

--- config/config.js
+++ config/config.js
@@ -16,11 +16,11 @@ module.exports = {
     logging: false
   },
   production: {
-    username: 'process.env.DB_USERNAME',
-    password: 'process.env.DB_PASSWORD',
-    database: 'process.env.DB_NAME',
-    host: 'process.env.DB_HOST',
-    port: 'process.env.DB_PORT',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql'
   },
   travis: {
